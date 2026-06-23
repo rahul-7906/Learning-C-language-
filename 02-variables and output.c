7da4dacdb9fc1fbd862e7393818e7e8529e51f6d
rahul-7906 <rahulmelkani4281@gmail.com>
2026-06-23 08:49:34 +0530

    Add files via upload
    Learning to create variables, know data types,printing output and moving to a next lind

---
+++ 02-variables and output.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int age = 17;
+	float pi= 3.14;
+	char star='*';
+	printf("age is %d\n",age);
+	printf("value of pi is %f\n",pi );
+	printf("character  is %c\n",star);
+return 0;
+}
