dc48e9da57867e35c0b762c5b168e4436495f4a0
rahul-7906 <rahulmelkani4281@gmail.com>
2026-06-23 12:26:55 +0530

    Add files via upload
    Learning about relational and logical operators

---
+++ 06-operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	//Relational operator
+	printf("%d\n", 5==2);
+	printf("%d\n", 5>2);
+	printf("%d\n", 5>=2);
+	printf("%d\n", 5<2);
+	printf("%d\n\n", 5<=2);
+	
+	
+	//Logical operator
+	printf("%d\n", (5>2)&&(6>=6));
+	printf("%d\n", (5>2)||(6>6));
+	
+	return 0;
+}
