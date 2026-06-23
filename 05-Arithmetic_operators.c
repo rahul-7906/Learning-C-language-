be40636c7227d92a92497ab48d78f85514a3bd66
rahul-7906 <rahulmelkani4281@gmail.com>
2026-06-23 11:23:39 +0530

    Add files via upload
    In this I learned to use arithmetic operations,their sequence, priority order etc.

---
+++ 05-Arithmetic_operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	//Arithenatic operators
+	/* Priority *,/,% 
+	                  +,-
+	                  =
+	  */
+
+	int a= 4*3/6*2 ;
+	printf("%d\n",a);
+	
+	int b = 5*4-10*3;
+	printf("%d",b);
+	return 0;
+}
