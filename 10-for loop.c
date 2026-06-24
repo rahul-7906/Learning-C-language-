d1267aee50cb2383c72f32b22457b8cd4755fa31
rahul-7906 <rahulmelkani4281@gmail.com>
2026-06-24 13:15:27 +0530

    Add files via upload

---
+++ 10-for loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//For loop
+int main(void) {
+    for(int i=0;i<50;i=i+1){
+    printf("Hello, World!\n");
+    }
+    // we can always use increment operator in place of i=i+1
+    // increment operator i++
+    // i++ (use then increase)
+    //++i (increase then use)
+
+    return 0;
+}
+
