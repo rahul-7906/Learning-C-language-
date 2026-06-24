d19d77bbc6ea4c77c82a0b65297acc8c1dd1b1ae
rahul-7906 <rahulmelkani4281@gmail.com>
2026-06-24 11:35:46 +0530

    Add files via upload
    I learned switch operator using a simple program to print days of a week .

---
+++ 09-switch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+	int day;
+	printf("Enter a day(1-7) : ");
+	//1=monday, 2= tuesday.....
+	scanf("%d",&day);
+	
+	switch(day){
+		case 1 : printf("Monday");
+		break;
+		case 2 : printf("Tuesday");
+		break;
+		case 3 : printf("Wednesday");
+		break;
+		case 4 : printf("Thursday");
+		break;
+		case 5 : printf("Friday");
+		break;
+		case 6 : printf("Saturday");
+		break;
+		case 7 : printf("Sunday");
+		break;
+		default : printf("Not a valid day");
+	}
+	return 0;
+}
